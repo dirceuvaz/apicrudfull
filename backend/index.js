5aaee8571b134ff7fd86f7f3fff37cdbf56f783e
Dirceu Vaz <dirceu.info@gmail.com>
2024-09-18 23:20:09 -0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,32 +2,43 @@ import express from 'express';
 import cors from 'cors';
 import compression from 'compression';
 
-//Imports para rotas
+// Imports para rotas
 import { loginUser } from './controllers/user.js';
-import userRoutes from './routes/users.js'; //usuarios
-import categoriasAnimaisRoutes from './routes/categoriasAnimais.js'; //categoria
-import animaisRoutes from './routes/animais.js'; //animais
-import voluntariosRoutes from './routes/voluntarios.js'; //voluntários
-import pedidoRoutes from './routes/pedidoRoutes.js'; //pedidos
-
+import userRoutes from './routes/users.js'; // usuários
+import categoriasAnimaisRoutes from './routes/categoriasAnimais.js'; // categorias
+import animaisRoutes from './routes/animais.js'; // animais
+import voluntariosRoutes from './routes/voluntarios.js'; // voluntários
+import pedidoRoutes from './routes/pedidoRoutes.js'; // pedidos
 
 const app = express();
 app.use(compression()); // Habilita compressão para todas as respostas
 app.use(express.json());
+
+const allowedOrigins = ['http://localhost:3000', 'https://apicrudfull.vercel.app'];
+
 app.use(cors({
-  origin: '*',
+  origin: function (origin, callback) {
+    // Permite requisições sem origem (como Postman ou CURL)
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  // Se você estiver usando cookies ou outros tipos de autenticação
+  credentials: true,
 }));
 
 app.use('/', userRoutes);  // Rota para usuários
 app.post('/login', loginUser);
 
-app.use('/categorias', categoriasAnimaisRoutes); // Rota para usuários
+app.use('/categorias', categoriasAnimaisRoutes); // Rota para categorias
 
 app.use('/animais', animaisRoutes); // Rota para animais
 
 app.use('/voluntarios', voluntariosRoutes);// Rota para voluntários
 
-app.use('/pedidos', pedidoRoutes);  // Rota para pediodos
+app.use('/pedidos', pedidoRoutes);  // Rota para pedidos
 
 // Start Server
 app.listen(8800, () => {
